e6b16026ad3c6a3875cadd1385cc7c1b1119e45c
martinunland <martin.e@unland.eu>
2023-04-20 16:04:28 +0200

    v0.1.1

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='pulse_mode_analysis',  # Replace with your desired library name
-    version='0.1.0',
-    description='Analysis of waveforms',
-    author='Martin Unland',  
-    author_email='martin.e@unland.eu',  
-    url='https://github.com/martinunland/pulse_mode_analysis',  
+    name="pulse_mode_analysis",  # Replace with your desired library name
+    version="0.1.1",
+    description="Analysis of waveforms",
+    author="Martin Unland",
+    author_email="martin.e@unland.eu",
+    url="https://github.com/martinunland/pulse_mode_analysis",
     packages=find_packages(),
     install_requires=[
         # List the packages required for your library here, e.g.
-         'scipy',
-         'numpy',
+        "scipy",
+        "numpy",
     ],
-    python_requires='>=3.6',
+    python_requires=">=3.6",
 )
